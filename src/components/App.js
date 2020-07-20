6967944a5fa08f00f703df209361f710d067beaa
Julia Seidman <juliaseid@gmail.com>
2020-07-20 10:22:30 -0700

    initial commit

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import Remedies from './Remedies';
+
+function App() {
+  return (
+    <React.Fragment>
+      < Remedies />
+    </React.Fragment>
+  );
+}
+
+export default App;
